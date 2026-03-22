5e370f3690512a4b87ab992580b13987a1ea12bc
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2026-03-22 15:06:01 +0900

    역할설정

---
+++ consumer2/src/main/java/consumer2/example/consumer2/Consumer2Application.java
@@ -0,0 +1,13 @@
+package consumer2.example.consumer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consumer2Application.class, args);
+	}
+
+}
